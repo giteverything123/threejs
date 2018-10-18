2073ad3e32d81af80ce6920cda8db381994e6b56
xuwenjun <156845109@qq.com>
2018-10-18 14:06:44 +0800

    开发框架初步搭建完成

---
+++ threejs/01/01-01/01-01.js
@@ -0,0 +1,3 @@
+var div=document.createElement('div');
+div.innerHTML='hello threejs';
+document.body.appendChild(div);
--- threejs/webpack.common.js
+++ threejs/webpack.common.js
@@ -1,10 +1,44 @@
 const path=require('path');
+const glob=require('glob');
 const htmlwebpackplugin=require('html-webpack-plugin');
 const cleanwebpackplugin=require('clean-webpack-plugin');
+let htmlOutputs=(()=>{
+	let result=[];
+	let pathArr=['./01/**/*.html'];
+	let pathReg=/^\.\/\d{2}\/\d{2}-\d{2}\/index\.html$/;
+	pathArr.forEach((pathItem)=>{
+		glob.sync(pathItem).forEach((entry)=>{
+			if(entry.match(pathReg)){
+				console.log(entry);
+				var titleReg=/\d{2}-\d{2}(?=\/index\.html)/;
+				var titlename=entry.match(titleReg)[0];
+				result.push(new htmlwebpackplugin({
+					template:entry,
+					filename:'index.html',
+					title:titlename
+				}));
+			}
+		});
+	});
+	return result;
+})();
+let entrys=(()=>{
+	let result={};
+	let pathArr=['./01/**/*.js'];
+	let pathReg=/^\.\/\d{2}\/\d{2}-\d{2}\/\d{2}-\d{2}\.js$/;
+	pathArr.forEach((pathItem)=>{
+		glob.sync(pathItem).forEach((entry)=>{
+			if(entry.match(pathReg)){
+				var nameReg=/\d{2}-\d{2}(?=\.js)/;
+				var name=entry.match(nameReg)[0];
+				result[name]=entry;
+			}
+		});
+	});
+	return result;
+})();
 module.exports={
-	entry:{
-		app:'./js/index.js'
-	},
+	entry:entrys,
 	output:{
 		filename:'[name].[hash:8].js',
 		chunkFilename:'[name].[hash:8].js',
@@ -21,12 +55,9 @@ module.exports={
 				use:['url-loader?limit=2048']
 			},
 		]
-	}
+	},
 	plugins:[
-		new htmlwebpackplugin({
-			title:'three',
-			template:'./index.html'
-		}),
+		...htmlOutputs,
 		new cleanwebpackplugin(['dist'])
 	],
 }
